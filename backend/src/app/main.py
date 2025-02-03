e7889fbb8cdc37cb7ef61949ebf9dc6038a8585d
devUtilityapp <dev.utility.app@gmail.com>
2025-02-03 19:58:49 +0900

    dev cors setting

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,5 +1,19 @@
 from .api import router
 from .core.config import settings
 from .core.setup import create_application
+from fastapi.middleware.cors import CORSMiddleware
 
 app = create_application(router=router, settings=settings)
+
+# CORS 미들웨어 설정
+origins = [
+    "http://localhost:5173",  # Vite 개발 서버
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
